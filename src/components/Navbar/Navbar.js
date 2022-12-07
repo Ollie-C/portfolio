4750cb717b6c2feabb5bfe8629c2eae71e4007b8
Ollie Cross <iio.olivercross@gmail.com>
2022-12-07 16:07:53 +0000

    Desktop and mobile navbar core complete

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { images } from "../../static";
 import "./Navbar.scss";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="navbar__links">
+        {["home", "about", "projects", "skills", "contact"].map((item) => (
+          <li className="navbar__link p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="navbar__menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 1.3, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {["home", "about", "projects", "skills", "contact"].map(
+                (item) => (
+                  <li key={item}>
+                    <a onClick={() => setToggle(false)} href={`#${item}`}>
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/static/images.js
@@ -0,0 +1,48 @@
+//Icons
+import api from "../assets/Icons/api.png";
+import css from "../assets/Icons/css.png";
+import email from "../assets/Icons/email.png";
+import git from "../assets/Icons/git.png";
+import html from "../assets/Icons/html.png";
+import javascript from "../assets/Icons/javascript.png";
+import node from "../assets/Icons/node.png";
+import python from "../assets/Icons/python.png";
+import react from "../assets/Icons/react.png";
+import sass from "../assets/Icons/sass.png";
+import typescript from "../assets/Icons/typescript.png";
+
+//Images
+import about01 from "../assets/Images/about01.png";
+import about02 from "../assets/Images/about02.png";
+import about03 from "../assets/Images/about03.png";
+import about04 from "../assets/Images/about04.png";
+import bgIMG from "../assets/Images/bgIMG.png";
+import bgWhite from "../assets/Images/bgWhite.png";
+import circle from "../assets/Images/circle.svg";
+import logo from "../assets/Images/logo.png";
+import mobile from "../assets/Images/mobile.png";
+import profile from "../assets/Images/profile.png";
+
+export default {
+  api,
+  css,
+  email,
+  git,
+  html,
+  javascript,
+  node,
+  python,
+  react,
+  sass,
+  typescript,
+  about01,
+  about02,
+  about03,
+  about04,
+  bgIMG,
+  bgWhite,
+  circle,
+  logo,
+  mobile,
+  profile,
+};
