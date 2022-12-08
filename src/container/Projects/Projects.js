7163b3fe364769dd9bde4780f019684d47e77d88
Ollie Cross <iio.olivercross@gmail.com>
2022-12-08 17:00:48 +0000

    Added layout with placeholder images. React communicating successfully with Sanity

--- src/container/Projects/Projects.js
+++ src/container/Projects/Projects.js
@@ -1,8 +1,112 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 import "./Projects.scss";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [projects, setProjects] = useState([]);
+  const [filteredProjects, setFilteredProjects] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "projects"]';
+
+    client.fetch(query).then((data) => {
+      setProjects(data);
+      setFilteredProjects(data);
+    });
+  }, []);
+
+  const handleProjectFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+      if (item === "All") {
+        setFilteredProjects(projects);
+      } else {
+        setFilteredProjects(
+          projects.filter((project) => project.tags.includes(item))
+        );
+      }
+    }, 500);
+  };
+
+  return (
+    <>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span> section
+      </h2>
+      <div className="portfolio__filter">
+        {["React", "UI/UX", "Web App", "All"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleProjectFilter(item)}
+            className={`portfolio__filter-item p-text ${
+              activeFilter === item ? "item-active" : " "
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="portfolio__main"
+      >
+        {filteredProjects.map((project, index) => (
+          <div className="portfolio__item" key={index}>
+            <div className="portfolio__img">
+              <img src={urlFor(project.imgUrl)} alt={project.name} />
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0,
+                }}
+                className="portfolio__hover"
+              >
+                <a href={project.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="portfolio__link"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={project.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="portfolio__link"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+            <div className="portfolio__content">
+              <h4 className="bold-text">{project.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {project.description}
+              </p>
+              <div className="portfolio__tag">
+                <p className="p-text">{project.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
 };
 
-export default Projects;
+export default AppWrap(Projects, "Projects");
